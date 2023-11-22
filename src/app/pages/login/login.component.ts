0a1475a63898c0eb1d8ba65c0ccbdab5b19c7c0c
adithyan-ms <adithyan.ms@thinkpalm.com>
2023-11-22 12:30:59 +0530

    added validation error and displayed

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm:FormGroup;
 
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username:['',[Validators.required,Validators.maxLength(15),Validators.minLength(3)]],
+      password:['',[Validators.required,Validators.pattern("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}")]]
+    })
+  }
+
+  onFormSubmit(form:FormGroup){
+    console.log(form);
+  }
 }
