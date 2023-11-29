4055f5f7358b73a3c3cb25f8ec0d047ddecbabea
adithyan-ms <adithyan.ms@thinkpalm.com>
2023-11-29 18:20:43 +0530

    added guard , show image in navbar

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,14 +1,13 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
-import { Router } from 'express';
-
+import { CanActivateFn, Router } from '@angular/router';
+ 
 export const authGuard: CanActivateFn = (route, state) => {
-
-  if(localStorage.getItem("token") && localStorage.getItem("token")){
-    console.log("in guard");
+  if(localStorage.getItem("token") && localStorage.getItem("user")){
     return true;
-  }
+  }else{
     const router = inject(Router)
-    return router.navigate(['/']);
-  
+    return router.navigate(['login']);
+  }
+ 
 };
+ 
