cd2c058ec6d9a590e930d08aa02e372d1b32af40
adithyan-ms <adithyan.ms@thinkpalm.com>
2023-11-29 18:59:33 +0530

    profile component added and routed, added functional guard for login and register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,48 @@
-import { Routes } from '@angular/router';
+import { Router, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { authGuard } from './guards/auth.guard';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { inject } from '@angular/core';
 
 export const routes: Routes = [
     {
         path:"login",
         component:LoginComponent,
-        title:"Login | ChatApp"
+        title:"Login | ChatApp",
+        canActivate:[()=>{
+            if(localStorage.getItem("token") && localStorage.getItem("user")){
+                const router = inject(Router)
+                return router.navigate(['home']);
+              }else{
+                return true;
+              }
+        }]
     },
     {
         path:"register",
         component:RegisterComponent,
-        title:"Register | ChatApp"
+        title:"Register | ChatApp",
+        canActivate:[()=>{
+            if(localStorage.getItem("token") && localStorage.getItem("user")){
+                const router = inject(Router)
+                return router.navigate(['home']);
+              }else{
+                return true;
+              }
+        }]
     },
     {
         path:"home",
         component:HomeComponent,
         title:"Home | ChatApp",
+        canActivate:[authGuard]
+    },
+    {
+        path:"profile",
+        component:ProfileComponent,
+        title:"Profile | ChatApp",
         canActivate:[authGuard]
     }
 ];
