547a73f5d8da31478e92e397850f60c669501c63
adithyan-ms <adithyan.ms@thinkpalm.com>
2023-11-30 19:25:31 +0530

    sidebar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { ApiService } from '../../services/api.service';
 import { error } from 'console';
 import { Observable } from 'rxjs';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ShowChatComponent } from './show-chat/show-chat.component';
 
 @Component({
     selector:"app-home",
     standalone:true,
-    imports:[FormsModule],
+    imports:[FormsModule,SidebarComponent,ShowChatComponent],
     templateUrl: './home.component.html',
     styleUrl:'./home.component.scss'
 })
---
+++ src/app/pages/home/show-chat/show-chat.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-show-chat',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './show-chat.component.html',
+  styleUrl: './show-chat.component.scss'
+})
+export class ShowChatComponent {
+
+}
