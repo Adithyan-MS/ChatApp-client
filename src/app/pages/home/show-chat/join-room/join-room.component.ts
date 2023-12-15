ad8e2d1d62b9970287337fad73fcf15186e8f1f8
adithyan-ms <adithyan.ms@thinkpalm.com>
2023-12-15 18:04:54 +0530

    working on dropup in messagecontainer

--- src/app/pages/home/show-chat/join-room/join-room.component.ts
+++ src/app/pages/home/show-chat/join-room/join-room.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RoomResultComponent } from './room-result/room-result.component';
+import { ApiService } from '../../../../services/api.service';
+import { environment } from '../../../../../environments/environment.development';
+import { Room } from '../../../../models/data-types';
 
 @Component({
   selector: 'app-join-room',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RoomResultComponent],
   templateUrl: './join-room.component.html',
   styleUrl: './join-room.component.scss'
 })
 export class JoinRoomComponent {
+
+  @ViewChild("roomNameValue") roomNameValue:ElementRef
+
+  room:Room|null
+  searchFlag:boolean=false
+  joinError:string
+
+  constructor(private api:ApiService){}
+
+  searchRoom(){
+    const roomName = this.roomNameValue.nativeElement.value
+    if(roomName!=''){
+      this.searchFlag=true
+      this.api.getReturn(`${environment.BASE_API_URL}/room/${roomName}`).subscribe((data:Room)=>{
+        this.room = data
+      },(error)=>{
+        this.room = null        
+      })
+    }
+    
+  }
+  getErrorMessage(event:any){
+    this.joinError=event
+  }
 
 }
---
+++ src/app/pages/home/show-chat/join-room/room-result/room-result.component.ts
@@ -0,0 +1,58 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Room } from '../../../../../models/data-types';
+import { environment } from '../../../../../../environments/environment.development';
+import { AppService } from '../../../../../services/app.service';
+import { ApiService } from '../../../../../services/api.service';
+import { DataService } from '../../../../../services/data.service';
+import { HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-room-result',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './room-result.component.html',
+  styleUrl: './room-result.component.scss'
+})
+export class RoomResultComponent implements OnInit{
+
+  @Input() roomData:Room
+  @Output() errorEvent = new EventEmitter<any>()
+  roomPic:string
+  errorMessage:string
+
+  constructor(private appService:AppService,private api:ApiService,private dataService:DataService){
+
+  }
+
+  ngOnInit(): void {
+    if(this.roomData.room_pic){      
+      this.roomPic = this.appService.getImageUrl(this.roomData.room_pic);
+    }else{
+      this.roomPic = environment.ROOM_IMAGE
+    }
+  }
+
+  joinRoom(){
+    const headers = new HttpHeaders().set("ResponseType","text")
+    this.api.postReturn(`${environment.BASE_API_URL}/room/${this.roomData.id}/join`,null,{headers}).subscribe((data)=>{
+      this.dataService.notifyOther({
+        view:"chat",
+        data:{
+          type:"room",
+          id:this.roomData.id,
+          name:this.roomData.name,
+          profile_pic:this.roomData.room_pic,
+          max_modified_at:this.roomData.modifiedAt
+        }
+      })
+      console.log(data)
+    },(error:any)=>{
+      console.log(error.error);      
+      this.errorMessage = JSON.parse(error["error"]).message;
+      this.errorEvent.emit(this.errorMessage)      
+    })
+  }
+
+
+}
