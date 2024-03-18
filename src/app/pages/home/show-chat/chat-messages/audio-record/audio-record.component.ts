d2fe27be5ab5795e7eb39b35bf20323a49742d97
adithyan-ms <adithyan.ms@thinkpalm.com>
2024-03-18 18:14:12 +0530

    audio recordRtc problem

--- src/app/pages/home/show-chat/chat-messages/audio-record/audio-record.component.ts
+++ src/app/pages/home/show-chat/chat-messages/audio-record/audio-record.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-audio-record',
@@ -8,6 +9,55 @@ import { CommonModule } from '@angular/common';
   templateUrl: './audio-record.component.html',
   styleUrl: './audio-record.component.scss'
 })
-export class AudioRecordComponent {
+export class AudioRecordComponent implements OnInit{
+
+  record:any
+  recording:boolean = false
+  url: any
+  error: any
+
+  constructor(private domSanitizer: DomSanitizer){}
+
+  sanitize(url: string){
+    return this.domSanitizer.bypassSecurityTrustUrl(url)
+  }
+
+  ngOnInit(): void {
+    if (this.record) {
+      return;
+    }
+
+    this.recording = true
+    let mediaConstraints = {
+       audio: true
+    }
+    navigator.mediaDevices
+      .getUserMedia(mediaConstraints)
+      .then(this.successCallback.bind(this), this.errorCallback.bind(this))
+  }
+
+  // startRecording(){}
+
+  successCallback(stream:any){
+    // var StereoAudioRecorder = RecordRTC.StereoAudioRecorder
+    // this.record = new StereoAudioRecorder(stream, {
+    //   type:"audio",
+    //   mimeType: "audio/wav"
+    // })
+    // this.record.record()
+  }
+
+  stopRecording(){
+    this.recording = false
+    this.record.stop(this.processRecording.bind(this))
+  }
+
+  processRecording(blob:any){
+    this.url = URL.createObjectURL(blob)
+  }
+
+  errorCallback(){
+    console.log("Can't play audio in your browser");    
+  }
 
 }
