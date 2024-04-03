484664df528cf790505446aff866371c244840b5
AdithyanMS <adithyan2ms@gmail.com>
2024-04-03 19:45:15 +0530

    no message

--- src/app/pages/home/show-chat/chat-messages/chat-messages.component.ts
+++ src/app/pages/home/show-chat/chat-messages/chat-messages.component.ts
@@ -102,14 +102,14 @@ export class ChatMessagesComponent implements OnInit,OnChanges,OnDestroy,AfterVi
       this.sendAudio(blob)
     });
 
-    var sockJsProtocols = ["xhr-streaming", "xhr-polling"];
-    this.stompClient =  Stomp.over(function(){
-      return new SockJS(`http://localhost:8080/ws`, null, {transports: sockJsProtocols});
-    });
+    var sockJsProtocols = ["websocket", "xhr-streaming", "xhr-polling"];
+    var sockJsClient = new SockJS(`http://localhost:8080/ws`, null, { transports: sockJsProtocols });
+    this.stompClient = Stomp.over(sockJsClient);
     
     this.stompClient.connect({}, (frame:any) => {
       console.log('connected to: ' + frame);
-    })
+    });
+    
   }
 
   ngOnDestroy(): void {
